fb36c8c6282878e42a59ed97d7e31bfe03e7cf40
trungtin03 <trungtintran1103@gmail.com>
2024-12-04 23:59:00 +0700

    add

--- QL_yeucau/Properties/AssemblyInfo.cs
+++ QL_yeucau/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3be73379-aeae-48e6-84ef-35aabbe580f4")]
+[assembly: Guid("c9eb85ae-80e7-409b-ab06-3c0e271c697e")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ QL_yeucau/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QL_yeucau.Startup))]
+namespace QL_yeucau
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
